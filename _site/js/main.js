a9d4cce781ae45804959ff8aa3d8d29d3f53e2ea
euonymblog <antoniamochan@gmail.com>
2017-05-25 13:20:28 +1000

    Web app for EUALF conference

---
+++ _site/js/main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function(){
+
+  $(".events").hide();
+
+  $(".date2jun").on("click", function(){
+    $(".events2jun").slideToggle("slow");
+  });
+
+  $(".date3jun").on("click", function(){
+    $(".events3jun").slideToggle("slow");
+  });
+
+  $(".date4jun").on("click", function(){
+    $(".events4jun").slideToggle("slow");
+    });
+
+  $(".date5jun").on("click", function(){
+    $(".events5jun").slideToggle("slow");
+    });
+
+  $(".date6jun").on("click", function(){
+    $(".events6jun").slideToggle("slow");
+    });
+
+});
