d5923e4d042091758d1a0488187c8bb25bd8618a
euonymblog <antoniamochan@gmail.com>
2017-05-31 16:46:14 +1000

    search

--- js/main.js
+++ js/main.js
@@ -24,3 +24,32 @@ $( document ).ready(function(){
 
 
 });
+$( document ).ready(function(){
+var $elementList = $('#myUL').find('li');
+  $('#searchInput').keyup(function(eve){
+		  searchString=$(this).val().toLowerCase();
+  searchArray=searchString.split(' ');
+  var len = searchArray.length;
+  $elementList.each(function(index,elem){
+        $eleli = $(elem)
+        $eleli.removeClass('hideThisLine');
+        var oneLine = $eleli.text().toLowerCase()
+        match = true,
+        sal = len;
+        while(sal--){
+          if( oneLine.indexOf( searchArray[sal] ) == -1 ){
+            match = false;
+          }
+        }
+        if(!match){
+          //console.log('this one is gets hidden',searchString);
+          $eleli.addClass('hideThisLine');
+        }
+      });
+      $('.dontShow').removeClass('dontShow');
+      $('.hideThisLine').addClass('dontShow');
+    });
+  $('#clearSearch').click(function (){
+    window.location.href=window.location.href;
+  });
+});
